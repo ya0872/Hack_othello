23c34bfb6797b57e03ee7290521f97d64558f5b2
Yamamoto Mizuki <yamamizu0322@icloud.com>
2025-10-21 18:23:45 +0900

    リセット機能追加

---
+++ my-counter-vite/src/utils/othello.ts
@@ -0,0 +1,9 @@
+import { type Board } from '../types';
+export const FirstOthelloBoard = (): Board =>{
+    const board: Board = Array(8).fill(null).map(() => Array(8).fill(null));
+    board[3][4] = 'black'
+    board[3][3] = 'white'
+    board[4][3] = 'black'
+    board[4][4] = 'white'
+    return board;
+};
